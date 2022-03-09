b466cb46f9e19eed7b7ee5fde76f665cde6a7687
KarlaYEP2 <karlerik2013@gmail.com>
2022-03-09 09:41:25 +0200

    Front-page
    Front-pag + about-page, yep

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.header').height($(window).height());
+})
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+document.querySelector("#go-to-aboutpage").addEventListener("click", function () {
+    window.location = "about_page.html"
+})
+
